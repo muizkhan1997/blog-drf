f42e39c92d91f7f9766a0188bdc6e87581765402
muizkhan1997 <70268792+muizkhan1997@users.noreply.github.com>
2023-06-13 16:18:18 +0500

    new changes

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,13 +1,44 @@
 # pylint: disable=missing-module-docstring
 from rest_framework import serializers
-
 from .models import Post
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = "__all__"
 
 
 class PostSerializer(serializers.ModelSerializer):
     """
-    This class serializes Client model.
+    This class serializes Post model.
     """
+
     class Meta:
         model = Post
         fields = "__all__"
+
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.content = validated_data.get("title", instance.title)
+        instance.title = validated_data.get("content", instance.content)
+        return instance
+
+class PostDetailedSerializer(serializers.ModelSerializer):
+    """
+    This class serializes Post model.
+    """
+
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ("id", "title", "content", "author")
+
+    def to_representation(self, instance):
+        response_dict = super().to_representation(instance)
+        return response_dict["title"]      
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import PostSerializer
+from .serializers import PostSerializer, PostDetailedSerializer
 from .models import Post
+from django.core.exceptions import ValidationError
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -10,7 +11,14 @@ from rest_framework.response import Response
 class PostAPIView(APIView):
     def get(self, request):
         posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        data = serializer.data
-        return Response(data, status=status.HTTP_200_OK)
+        serializer = PostDetailedSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            raise ValidationError(serializer.errors)
     
